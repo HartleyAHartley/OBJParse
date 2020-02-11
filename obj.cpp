c8777031098e48c63e0ea109e92b7286dd62684e
Alice Hartley <alicehartleyhartley@gmail.com>
2020-02-11 13:02:28 -0500

    More face formats and fuzzer tests

--- obj.cpp
+++ obj.cpp
@@ -14,6 +14,14 @@ struct mesh{
   std::vector<glm::vec3> vertexnormals;
 };
 
+struct indexSet {
+  int iv;
+  int ivt;
+  int ivn;
+};
+
+size_t linenumber = 0;
+
 const std::set<std::string> supportedtokens{
   "v","vt","f","o","vn","#"
 };
@@ -28,90 +36,199 @@ const std::set<std::string> unsupportedtokens{
   "trace_obj","ctech","stech","call","scmp","csh"
 };
 
-bool empty(std::istringstream& tail){
-  return tail.rdbuf()->in_avail() == 0;
-}
 std::string tailstr(std::istringstream& tail){
   return tail.rdbuf()->str();
+}
+bool isempty(std::istringstream& tail){
+  return tail.rdbuf()->in_avail() == 0;
+}
+void empty(std::istringstream& tail){
+  if(!isempty(tail)){
+    throw "Extra characters at the end of the line: '" + tailstr(tail) + "'";
+  }
+}
+inline bool check(std::istream& is){
+  if(!is){
+    return false;
+  }
+  return true;
+}
+inline void pass(std::istream& is,  std::string s){
+  if(!check(is)){
+    throw s;
+  }
+}
+inline void fail(std::istream& is, std::string s){
+  if(check(is)){
+    throw s;
+  }
 }
 
 std::string parse_o(std::istringstream& tail){
   std::string name;
-  if (!(tail >> name)) {
-     throw "No object name specified"; 
-  }
-  if(!empty(tail)){
-    throw "Extra characters at the end of the line: '" + tailstr(tail) + "'";
-  }
+  pass((tail >> name),"No object name specified"); 
+  empty(tail);
   return name;
 }
 
 glm::vec3 parse_v(std::istringstream& tail){
   float x,y,z,w;
-  if (!(tail >> x >> y >> z)) {
-     throw "Invalid Vertex line format"; 
-  }
-  if((tail >> w)) { 
-    throw "w vertex parameter not supported";
-  }
-  if(!empty(tail)){
-    throw "Extra characters at the end of the line: '" + tailstr(tail) + "'";
-  }
+  pass((tail >> x >> y >> z),"Invalid Vertex line format");
+  fail((tail >> w),"w vertex parameter not supported");
+  empty(tail);
   return {x,y,z};
 }
 
 glm::vec2 parse_vt(std::istringstream& tail){
   float a,b;
-  if (!(tail >> a >> b)) {
-     throw "Invalid Vertex Texture line format"; 
-  }
-  if(!empty(tail)){
-    throw "Extra characters at the end of the line: '" + tailstr(tail) + "'";
-  }
+  pass((tail >> a >> b),"Invalid Vertex Texture line format"); 
+  empty(tail);
   return {a,b};
 }
 
 glm::vec3 parse_vn(std::istringstream& tail){
   float x,y,z;
-  if (!(tail >> x >> y >> z)) {
-     throw "Invalid Vertex Texture line format"; 
-  }
-  if(!empty(tail)){
-    throw "Extra characters at the end of the line: '" + tailstr(tail) + "'";
-  }
+  pass((tail >> x >> y >> z),"Invalid Vertex Texture line format"); 
+  empty(tail);
   return {x,y,z};
 }
 
-struct indices {
+enum f_type {
+  none,
+  v,
+  v_vt,
+  v_vn,
+  v_vt_vn
+};
+
+void slash(char slash){
+  if(slash != '/'){
+    throw "f dividing character should be a forward slash instead was a: " + std::string(1,slash);
+  }
+}
+
+bool ftype_v(std::string fstr, int& iv){
+  std::istringstream fstrstream(fstr);
+  if(check((fstrstream >> iv))){
+    if(isempty(fstrstream)){
+      return true;
+    }
+  } 
+  return false;
+}
+
+bool ftype_v_vt(std::string fstr, int& iv, int& ivt){
+  char div = '\0';
+  std::istringstream fstrstream(fstr);
+  if(check((fstrstream >> iv >> div >> ivt))){
+    if(isempty(fstrstream)){
+      slash(div);
+      return true;
+    }
+  }
+  return false;
+}
+
+bool ftype_v_vn(std::string fstr, int& iv, int& ivn){
+  char div[2] = {'\0','\0'};
+  std::istringstream fstrstream(fstr);
+  if(check((fstrstream >> iv >> div[0] >> div[1] >> ivn))){
+    if(isempty(fstrstream)){
+      slash(div[0]);
+      slash(div[1]);
+      return true;
+    }
+  }
+  return false;
+}
+
+bool ftype_v_vt_vn(std::string fstr, int& iv, int& ivt, int& ivn){
+  char div[2] = {'\0','\0'};
+  std::istringstream fstrstream(fstr);
+  if(check((fstrstream >> iv >> div[0] >> ivt >> div[1] >> ivn))){
+    slash(div[0]);
+    slash(div[1]);
+    empty(fstrstream);
+    return true;
+  }
+  return false;
+}
+
+f_type ftype(std::string fstr, int& iv, int& ivt, int& ivn){
+  if(ftype_v(fstr,iv)){
+    return v;
+  }else if(ftype_v_vt(fstr,iv,ivt)){
+    return v_vt;
+  }else if(ftype_v_vn(fstr,iv,ivn)){
+    return v_vn;
+  }else if(ftype_v_vt_vn(fstr,iv,ivt,ivn)){
+    return v_vt_vn;
+  }
+  throw "Invalid f mode";
+}
+
+void checkzeroes(f_type fmode, int iv, int ivt, int ivn){
+  if(iv == 0 || (ivt == 0 && fmode != v_vn) || (ivn == 0 && fmode != v_vt)){
+    throw "Indexing starts at 1";
+  }
+}
+
+std::vector<indexSet> parse_f(std::istringstream& tail, f_type& fmode){
+  std::string fstr;
+  std::vector<indexSet> pairs;
+
   int iv;
   int ivt;
   int ivn;
-};
-//in progess
-std::vector<indices> parse_f(std::istringstream& tail){
-  std::string fstr;
-  std::vector<indices> pairs;
+  tail >> fstr;
+  fmode = ftype(fstr,iv,ivt,ivn);
+  checkzeroes(fmode,iv,ivt,ivn);
+  pairs.push_back({iv-1,ivt-1,ivn-1});
   while((tail >> fstr)){
     std::istringstream fstrstream(fstr);
-    char slash;
-    int iv,ivt,ivn;
-    if(!(fstrstream >> iv >> slash >> ivt >> slash >> ivn)){ throw "unimplemented"; }
+    char slashes[2] = {'/','/'};
+    switch(fmode){
+      case v:
+        pass((fstrstream >> iv),"Invalid v mode");
+        break;
+      case v_vt:
+        pass((fstrstream >> iv >> slashes[0] >> ivt),"Invalid v_vt mode");
+        break;
+      case v_vn:
+        pass((fstrstream >> iv >> slashes[0] >> slashes[1] >> ivn),"Invalid v_vn mode");
+        break;
+      case v_vt_vn:
+        pass((fstrstream >> iv >> slashes[0] >> ivt >> slashes[1] >> ivn),"Invalid v_vt_vn mode");
+        break;
+      case none:
+        throw "Invalid f mode";
+        break;
+    }
+    slash(slashes[0]);
+    slash(slashes[1]);
+    empty(fstrstream);
+    checkzeroes(fmode,iv,ivt,ivn);
     pairs.push_back({iv-1,ivt-1,ivn-1});
+  }
+  if(pairs.size() < 3){
+    throw "f must have at least 3 elements";
   }
   return pairs;
 }
 
 bool getfullline(std::istream& in, std::string& line){
   std::string buffer;
-  if(!std::getline(std::cin, buffer)){
+  if(!std::getline(in, buffer)){
     return false;
   }
+  linenumber++;
   while(buffer.ends_with('\\')){
     buffer.pop_back();
     std::string addbuffer;
     if(!std::getline(std::cin, addbuffer)){
       throw "eof reached while combining lines";
     }
+    linenumber++;
     buffer += addbuffer;
   }
   line = buffer;
@@ -119,13 +236,26 @@ bool getfullline(std::istream& in, std::string& line){
 }
 
 
-std::vector<mesh> loadobj(){
+size_t getindex(long maxIndex, long pos){
+  if(pos > maxIndex){
+    throw "Index greater than maxIndex: " + std::to_string(pos) + " > " + std::to_string(maxIndex);
+  }
+  if(pos >= 0){
+    return pos-1;
+  }
+  if(maxIndex - pos >= 0){
+    return maxIndex-pos;
+  }
+  throw "Index references a vertex beyond 0";
+}
+
+std::vector<mesh> loadObj(std::istream& in){
   std::string line;
   std::vector<glm::vec3> iverts;
   std::vector<glm::vec2> itexs;
   std::vector<glm::vec3> inorms;
   std::vector<mesh> model;
-  while (getfullline(std::cin,line)){
+  while (getfullline(in,line)){
     std::istringstream iss(line);
     std::string linetoken;
     if(!(iss >> linetoken)){ 
@@ -148,37 +278,70 @@ std::vector<mesh> loadobj(){
     } else if(linetoken == "vn"){
       inorms.push_back(parse_vn(iss));
     } else if(linetoken == "f"){
-      std::vector<indices> pairs = parse_f(iss);
-      try{
-        for(int i = 0; i < pairs.size()-2; i++){
-          model.back().vertices.insert(end(model.back().vertices),{
-            iverts.at(pairs[0].iv),iverts.at(pairs[i+1].iv),iverts.at(pairs[i+2].iv)
-          });
+      f_type fmode;
+      std::vector<indexSet> pairs = parse_f(iss, fmode);
+      size_t ivertsize = iverts.size();
+      size_t itexsize = itexs.size();
+      size_t inormsize = inorms.size();
+      for(size_t i = 0; i < pairs.size()-2; i++){
+        model.back().vertices.insert(end(model.back().vertices),{
+          iverts[getindex(ivertsize,pairs[0].iv)],
+          iverts[getindex(ivertsize,pairs[i+1].iv)],
+          iverts[getindex(ivertsize,pairs[i+2].iv)]
+        });
+        if(fmode == v_vt || fmode == v_vt_vn){
           model.back().texturecoordinates.insert(end(model.back().texturecoordinates),{
-            itexs.at(pairs[0].ivt),itexs.at(pairs[i+1].ivt),itexs.at(pairs[i+2].ivt)
-          });
-          model.back().vertexnormals.insert(end(model.back().vertexnormals),{
-            inorms.at(pairs[0].ivn),inorms.at(pairs[i+1].ivn),inorms.at(pairs[i+2].ivn)
+            itexs[getindex(itexsize,pairs[0].ivt)],
+            itexs[getindex(itexsize,pairs[i+1].ivt)],
+            itexs[getindex(itexsize,pairs[i+2].ivt)]
           });
         }
-      }catch(std::out_of_range){
-        throw "Invalid element index";
+        if(fmode == v_vn || fmode == v_vt_vn){
+          model.back().vertexnormals.insert(end(model.back().vertexnormals),{
+            inorms[getindex(inormsize,pairs[0].ivn)],
+            inorms[getindex(inormsize,pairs[i+1].ivn)],
+            inorms[getindex(inormsize,pairs[i+2].ivn)]
+          });
+        }
       }
     }
   }
   return model;
 }
 
-int main() {
+int main(int argc, char** argv){
+#ifndef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
   std::vector<mesh> model;
-  try{
-    model = loadobj();
-  }catch(const char* s){
-    std::cout << s << std::endl;
-    return 1;
-  }catch(std::string s){
-    std::cout << s << std::endl;
-    return 1; 
+  if(argc > 2){
+    if(argc > 1){
+      std::fstream obj(argv[1], std::ios::in);
+      if(obj.is_open()){
+        model = loadObj(obj);
+      }else{
+        throw "Failed to open file";
+      }
+    }else{
+      model = loadObj(std::cin);
+    }
+  }else {
+    try{
+      if(argc > 1){
+        std::fstream obj(argv[1], std::ios::in);
+        if(obj.is_open()){
+          model = loadObj(obj);
+        }else{
+          throw "Failed to open file";
+        }
+      }else{
+        model = loadObj(std::cin);
+      }
+    }catch(const char* s){
+      std::cout << "At line: " << linenumber << ": " << s << std::endl;
+      return 1;
+    }catch(std::string s){
+      std::cout << "At line: " << linenumber << ": " << s << std::endl;
+      return 1; 
+    }
   }
   for(const auto & m : model){
     std::cout << m.meshName << std::endl;
@@ -186,5 +349,25 @@ int main() {
     std::cout << m.vertexnormals.size() << std::endl;
     std::cout << m.vertices.size() << std::endl;
   }
+#endif
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+  while (__AFL_LOOP(10000)) {
+    size_t linenumber = 0;
+    try{
+      for(const auto & m : loadObj(std::cin)){
+        std::cout << m.meshName << std::endl;
+        std::cout << m.texturecoordinates.size() << std::endl;
+        std::cout << m.vertexnormals.size() << std::endl;
+        std::cout << m.vertices.size() << std::endl;
+      }
+    }catch(const char* s){
+      std::cout << "At line: " << linenumber << ": " << s << std::endl;
+      return 1;
+    }catch(std::string s){
+      std::cout << "At line: " << linenumber << ": " << s << std::endl;
+      return 1; 
+    }
+  }
+#endif
   return 0;
 }
